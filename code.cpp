6d34b7af7a54ce631ce4615384b3666f9549f9e7
nguyen xuan tien <tienveo102@gmail.com>
2023-03-21 23:09:21 +0700

    phien ban 2

--- code.cpp
+++ code.cpp
@@ -3,8 +3,12 @@ void sap_xep_day_so(int* a, int n)
 {
 // ma lenh sap xep day so
 }
+int tim_max(int* a, int n)
+{
+// ma lenh ham tim max
+}
 int main()
 {
-cout << "Phien ban dau tien";
+cout << "Phien ban 2";
 return 0;
 }
--- code.h
+++ code.h
@@ -1,3 +1,4 @@
 #include <iostream>
 using namespace std;
 void sap_xep_day_so(int* a, int n);
+int tim_max(int* a, int n);
